33477c4fe29246640fbd4244a4a6cbad9cc6658a
Cristhian Manzano <crisven1998@gmail.com>
2021-12-15 14:16:12 -0500

    Adding resources

---
+++ firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/fireStore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYDjPQyXAihiELK39Llfs1Zw62AQOp8R0",
+  authDomain: "house-marketplace-1b743.firebaseapp.com",
+  projectId: "house-marketplace-1b743",
+  storageBucket: "house-marketplace-1b743.appspot.com",
+  messagingSenderId: "565901040289",
+  appId: "1:565901040289:web:83b246431b3889494aaee0",
+};
+
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
